0d600c9e0e751fe18198dc82017aaf06c881dc2f
nguyenquocviet1002 <vietnguyen10022001@gmail.com>
2023-10-30 09:58:08 +0700

    update js header

--- Home/header_kn_2_0_0/js/header_kn_2_0_0.js
+++ Home/header_kn_2_0_0/js/header_kn_2_0_0.js
@@ -1,3 +1,46 @@
+const data = [
+    {
+        label: 'dropdown-1',
+        layout: '<li class="header_kn_2_0_0__head"><span class="header_kn_2_0_0__back">&#10094;</span>Dịch vụ </li> <li class="header_kn_2_0_0__dropdownItem"><a href="#">Thẩm mỹ Mắt1</a></li> <li class="header_kn_2_0_0__dropdownItem"><a href="#">Thẩm mỹ Mũi</a></li> <li class="header_kn_2_0_0__dropdownItem"><a href="#">Thẩm mỹ Hàm mặt</a></li>'
+    },
+    {
+        label: 'dropdown-2',
+        layout: '<li class="header_kn_2_0_0__head"><span class="header_kn_2_0_0__back">&#10094;</span>Dịch vụ </li> <li class="header_kn_2_0_0__dropdownItem"><a href="#">Thẩm mỹ Mắt2</a></li> <li class="header_kn_2_0_0__dropdownItem"><a href="#">Thẩm mỹ Mũi</a></li> <li class="header_kn_2_0_0__dropdownItem"><a href="#">Thẩm mỹ Hàm mặt</a></li>'
+    },
+    {
+        label: 'dropdown-3',
+        layout: '<li class="header_kn_2_0_0__head"><span class="header_kn_2_0_0__back">&#10094;</span>Dịch vụ </li> <li class="header_kn_2_0_0__dropdownItem"><a href="#">Thẩm mỹ Mắt3</a></li> <li class="header_kn_2_0_0__dropdownItem"><a href="#">Thẩm mỹ Mũi</a></li> <li class="header_kn_2_0_0__dropdownItem"><a href="#">Thẩm mỹ Hàm mặt</a></li>'
+    },
+]
+
+const elmHover = document.querySelectorAll('.header_kn_2_0_0__navItem a');
+const elmBg = document.querySelector('.header_kn_2_0_0__bg');
+for(let i = 0; i < elmHover.length; i++){
+    elmHover[i].addEventListener('mouseover', () => {
+        for(let i = 0; i < elmHover.length; i++){
+            const elmId = elmHover[i].getAttribute('data-id');
+            if(elmId !== null){
+                document.getElementById(elmId).innerHTML = '';
+            }
+        }
+        const elmId = elmHover[i].getAttribute('data-id');
+        const innerData = data.filter(item => item.label === elmId);
+        if(innerData.length > 0){
+            document.getElementById(elmId).innerHTML = innerData[0].layout;
+            elmBg.style.display = 'block';
+        }
+    })
+}
+elmBg.addEventListener('mouseover', () => {
+    for(let i = 0; i < elmHover.length; i++){
+        const elmId = elmHover[i].getAttribute('data-id');
+        if(elmId !== null){
+            document.getElementById(elmId).innerHTML = '';
+            elmBg.style.display = 'none';
+        }
+    }
+})
+
 document.querySelector('.header_kn_2_0_0__button').addEventListener('click', () => {
     document.querySelector('.header_kn_2_0_0__bottom').classList.add('show');
     const elmClickDown = document.querySelectorAll('.header_kn_2_0_0__bottom.show .header_kn_2_0_0__navItem a');
