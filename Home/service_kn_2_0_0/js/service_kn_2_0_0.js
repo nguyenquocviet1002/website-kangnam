2cdbffd44b28a010a1f3f147ab6e41b652cbea27
quangphe <dd>
2023-10-30 17:25:49 +0700

    update service

--- Home/service_kn_2_0_0/js/service_kn_2_0_0.js
+++ Home/service_kn_2_0_0/js/service_kn_2_0_0.js
@@ -1,37 +1,55 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var items = document.querySelectorAll(".service_kn_2_0_0__item");
+const data = [
+    {
+        label: 'dropdown-1',
+        layout: '<a href="" class="service_kn_2_0_0__boxHover"><div class="service_kn_2_0_0__content"><h3 class="service_kn_2_0_0__titleChild titleHover">Chuyên khoa</h3><h2 class="service_kn_2_0_0__text textHover">Thẩm mỹ khuôn mặt</h2><ul class="service_kn_2_0_0__list"><li>Chỉnh hàm hô móm</li><li>Hạ gò má 3D</li><li>Gọt hàm V-line</li><li>Độn cằm V-line</li></ul></div><div class="service_kn_2_0_0__btn btnHover"><span>→</span> Xem thêm</div></a>'
+    },
+    {
+        label: 'dropdown-2',
+        layout: '<a href="" class="service_kn_2_0_0__boxHover"><div class="service_kn_2_0_0__content"><h3 class="service_kn_2_0_0__titleChild titleHover">Chuyên khoa</h3><h2 class="service_kn_2_0_0__text textHover">Thẩm mỹ khuôn mặt</h2><ul class="service_kn_2_0_0__list"><li>Chỉnh hàm hô móm</li><li>Hạ gò má 3D</li><li>Gọt hàm V-line</li><li>Độn cằm V-line</li></ul></div><div class="service_kn_2_0_0__btn btnHover"><span>→</span> Xem thêm</div></a>'
+    },
+    {
+        label: 'dropdown-3',
+        layout: '<a href="" class="service_kn_2_0_0__boxHover"><div class="service_kn_2_0_0__content"><h3 class="service_kn_2_0_0__titleChild titleHover">Chuyên khoa</h3><h2 class="service_kn_2_0_0__text textHover">Thẩm mỹ khuôn mặt</h2><ul class="service_kn_2_0_0__list"><li>Chỉnh hàm hô móm</li><li>Hạ gò má 3D</li><li>Gọt hàm V-line</li><li>Độn cằm V-line</li></ul></div><div class="service_kn_2_0_0__btn btnHover"><span>→</span> Xem thêm</div></a>'
+    },
+    {
+        label: 'dropdown-4',
+        layout: '<a href="" class="service_kn_2_0_0__boxHover"><div class="service_kn_2_0_0__content"><h3 class="service_kn_2_0_0__titleChild titleHover">Chuyên khoa</h3><h2 class="service_kn_2_0_0__text textHover">Thẩm mỹ khuôn mặt</h2><ul class="service_kn_2_0_0__list"><li>Chỉnh hàm hô móm</li><li>Hạ gò má 3D</li><li>Gọt hàm V-line</li><li>Độn cằm V-line</li></ul></div><div class="service_kn_2_0_0__btn btnHover"><span>→</span> Xem thêm</div></a>'
+    },
+    {
+        label: 'dropdown-5',
+        layout: '<a href="" class="service_kn_2_0_0__boxHover"><div class="service_kn_2_0_0__content"><h3 class="service_kn_2_0_0__titleChild titleHover">Chuyên khoa</h3><h2 class="service_kn_2_0_0__text textHover">Thẩm mỹ khuôn mặt</h2><ul class="service_kn_2_0_0__list"><li>Chỉnh hàm hô móm</li><li>Hạ gò má 3D</li><li>Gọt hàm V-line</li><li>Độn cằm V-line</li></ul></div><div class="service_kn_2_0_0__btn btnHover"><span>→</span> Xem thêm</div></a>'
+    },
+]
 
-    items.forEach(function (item) {
-        var itemBox = item.querySelector(".service_kn_2_0_0__itemBox");
-
-        item.addEventListener("mouseover", function () {
-            var newItemHover = document.createElement("div");
-            newItemHover.classList.add("service_kn_2_0_0__itemHover");
-            newItemHover.innerHTML = `
-                <a href="" class="service_kn_2_0_0__boxHover">
-                    <div class="service_kn_2_0_0__content">
-                        <h3 class="service_kn_2_0_0__titleChild titleHover">Chuyên khoa</h3>
-                        <h2 class="service_kn_2_0_0__text textHover">Thẩm mỹ khuôn mặt</h2>
-                        <ul class="service_kn_2_0_0__list">
-                            <li>Chỉnh hàm hô móm</li>
-                            <li>Hạ gò má 3D</li>
-                            <li>Gọt hàm V-line</li>
-                            <li>Độn cằm V-line</li>
-                        </ul>
-                    </div>
-                    <div class="service_kn_2_0_0__btn btnHover">
-                        <span>→</span> Xem thêm
-                    </div>
-                </a>
-            `;
-            itemBox.insertAdjacentElement("afterend", newItemHover);
-        });
-
-        item.addEventListener("mouseout", function () {
-            var insertedItemHover = item.querySelector(".service_kn_2_0_0__itemHover");
-            if (insertedItemHover) {
-                insertedItemHover.remove();
+const eventService = () => {
+    const elmHover = document.querySelectorAll('.service_kn_2_0_0__item');
+    const elmBg = document.querySelector('.service_kn_2_0_0__bg');
+    for (let i = 0; i < elmHover.length; i++) {
+        elmHover[i].addEventListener('mouseover', () => {
+            for (let i = 0; i < elmHover.length; i++) {
+                const elmId = elmHover[i].getAttribute('data-id');
+                if (elmId !== null) {
+                    document.getElementById(elmId).innerHTML = '';
+                }
             }
-        });
-    });
-});
+            const elmId = elmHover[i].getAttribute('data-id');
+            const innerData = data.filter(item => item.label === elmId);
+            if (innerData.length > 0) {
+                document.getElementById(elmId).innerHTML = innerData[0].layout;
+                elmBg.style.display = 'block';
+            }
+        })
+    }
+    elmBg.addEventListener('mouseover', () => {
+        for (let i = 0; i < elmHover.length; i++) {
+            const elmId = elmHover[i].getAttribute('data-id');
+            if (elmId !== null) {
+                document.getElementById(elmId).innerHTML = '';
+                elmBg.style.display = 'none';
+            }
+        }
+    })
+}
+
+eventService();
+
