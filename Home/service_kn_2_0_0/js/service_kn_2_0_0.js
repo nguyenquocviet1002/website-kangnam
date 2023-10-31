7f4bbf08ca031801b5a41ddd553a3734c024d211
quangphe <dd>
2023-10-31 10:51:20 +0700

    quang update animation

--- Home/service_kn_2_0_0/js/service_kn_2_0_0.js
+++ Home/service_kn_2_0_0/js/service_kn_2_0_0.js
@@ -1,26 +1,3 @@
-const data = [
-    {
-        label: 'dropdown-1',
-        layout: '<a href="" class="service_kn_2_0_0__boxHover"><div class="service_kn_2_0_0__content"><h3 class="service_kn_2_0_0__titleChild titleHover">Chuyên khoa</h3><h2 class="service_kn_2_0_0__text textHover">Thẩm mỹ khuôn mặt</h2><ul class="service_kn_2_0_0__list"><li>Chỉnh hàm hô móm</li><li>Hạ gò má 3D</li><li>Gọt hàm V-line</li><li>Độn cằm V-line</li></ul></div><div class="service_kn_2_0_0__btn btnHover"><span>→</span> Xem thêm</div></a>'
-    },
-    {
-        label: 'dropdown-2',
-        layout: '<a href="" class="service_kn_2_0_0__boxHover"><div class="service_kn_2_0_0__content"><h3 class="service_kn_2_0_0__titleChild titleHover">Chuyên khoa</h3><h2 class="service_kn_2_0_0__text textHover">Thẩm mỹ khuôn mặt</h2><ul class="service_kn_2_0_0__list"><li>Chỉnh hàm hô móm</li><li>Hạ gò má 3D</li><li>Gọt hàm V-line</li><li>Độn cằm V-line</li></ul></div><div class="service_kn_2_0_0__btn btnHover"><span>→</span> Xem thêm</div></a>'
-    },
-    {
-        label: 'dropdown-3',
-        layout: '<a href="" class="service_kn_2_0_0__boxHover"><div class="service_kn_2_0_0__content"><h3 class="service_kn_2_0_0__titleChild titleHover">Chuyên khoa</h3><h2 class="service_kn_2_0_0__text textHover">Thẩm mỹ khuôn mặt</h2><ul class="service_kn_2_0_0__list"><li>Chỉnh hàm hô móm</li><li>Hạ gò má 3D</li><li>Gọt hàm V-line</li><li>Độn cằm V-line</li></ul></div><div class="service_kn_2_0_0__btn btnHover"><span>→</span> Xem thêm</div></a>'
-    },
-    {
-        label: 'dropdown-4',
-        layout: '<a href="" class="service_kn_2_0_0__boxHover"><div class="service_kn_2_0_0__content"><h3 class="service_kn_2_0_0__titleChild titleHover">Chuyên khoa</h3><h2 class="service_kn_2_0_0__text textHover">Thẩm mỹ khuôn mặt</h2><ul class="service_kn_2_0_0__list"><li>Chỉnh hàm hô móm</li><li>Hạ gò má 3D</li><li>Gọt hàm V-line</li><li>Độn cằm V-line</li></ul></div><div class="service_kn_2_0_0__btn btnHover"><span>→</span> Xem thêm</div></a>'
-    },
-    {
-        label: 'dropdown-5',
-        layout: '<a href="" class="service_kn_2_0_0__boxHover"><div class="service_kn_2_0_0__content"><h3 class="service_kn_2_0_0__titleChild titleHover">Chuyên khoa</h3><h2 class="service_kn_2_0_0__text textHover">Thẩm mỹ khuôn mặt</h2><ul class="service_kn_2_0_0__list"><li>Chỉnh hàm hô móm</li><li>Hạ gò má 3D</li><li>Gọt hàm V-line</li><li>Độn cằm V-line</li></ul></div><div class="service_kn_2_0_0__btn btnHover"><span>→</span> Xem thêm</div></a>'
-    },
-]
-
 const eventService = () => {
     const elmHover = document.querySelectorAll('.service_kn_2_0_0__item');
     const elmBg = document.querySelector('.service_kn_2_0_0__bg');
