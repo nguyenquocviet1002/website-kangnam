0f8f0f6ebf9652650e29633bf818ffa6ba87ee7f
nguyenquocviet1002 <vietnguyen10022001@gmail.com>
2023-11-03 11:16:27 +0700

    update page home

--- Home/doctor_kn_2_0_0/js/doctor_kn_2_0_0_data.js
+++ Home/doctor_kn_2_0_0/js/doctor_kn_2_0_0_data.js
@@ -1,51 +1,51 @@
 const doctors = [
   {
-    name: "Bác sĩ 1",
-    image: `${doctor_kn_2_0_0_url}images/bacsi-small.webp`,
+    name: "richard huy",
+    image: `${doctor_kn_2_0_0_url}images/bacsi-small1.webp`,
     picture: `${doctor_kn_2_0_0_url}images/bacsi.webp`,
-    level: "Thạc sĩ - Bác sĩ",
-    position: "Giám đốc chuyên môn Da liễu",
+    level: "tiến sĩ - Bác sĩ",
+    position: "Giám đốc chuyên môn pttm kangnam",
     note: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat ratione voluptatum ipsum assumenda, reiciendis molestias tenetur dolor voluptates sit optio ex repellendus inventore veritatis. Quos sunt maxime recusandae debitis incidunt?"
   },
   {
-    name: "Bác sĩ 2",
-    image: `${doctor_kn_2_0_0_url}images/bacsi-small.webp`,
+    name: "kim jeong hwang",
+    image: `${doctor_kn_2_0_0_url}images/bacsi-small2.webp`,
     picture: `${doctor_kn_2_0_0_url}images/bacsi.webp`,
-    level: "Thạc sĩ - Bác sĩ",
-    position: "Giám đốc chuyên môn Da liễu",
+    level: "giáo sư - Bác sĩ",
+    position: "cố vấn chuyên môn hệ thống",
     note: "Nội dung 2"
   },
   {
-    name: "Bác sĩ 3",
-    image: `${doctor_kn_2_0_0_url}images/bacsi-small.webp`,
+    name: "henry nguyễn",
+    image: `${doctor_kn_2_0_0_url}images/bacsi-small3.webp`,
     picture: `${doctor_kn_2_0_0_url}images/bacsi.webp`,
-    level: "Thạc sĩ - Bác sĩ",
-    position: "Giám đốc chuyên môn Da liễu",
+    level: "bác sĩ ck1",
+    position: "phó khoa PTTM gương mặt ",
     note: "Nội dung 3"
   },
   {
-    name: "Bác sĩ 4",
-    image: `${doctor_kn_2_0_0_url}images/bacsi-small.webp`,
+    name: "thủy lê",
+    image: `${doctor_kn_2_0_0_url}images/bacsi-small4.webp`,
     picture: `${doctor_kn_2_0_0_url}images/bacsi.webp`,
-    level: "Thạc sĩ - Bác sĩ",
-    position: "Giám đốc chuyên môn Da liễu",
+    level: "Bác sĩ",
+    position: "trưởng khoa da liễu",
     note: "Nội dung 4"
   },
   {
-    name: "Bác sĩ 5",
-    image: `${doctor_kn_2_0_0_url}images/bacsi-small.webp`,
+    name: "Jolie huỳnh",
+    image: `${doctor_kn_2_0_0_url}images/bacsi-small5.webp`,
     picture: `${doctor_kn_2_0_0_url}images/bacsi.webp`,
-    level: "Thạc sĩ - Bác sĩ",
-    position: "Giám đốc chuyên môn Da liễu",
+    level: "thạc sĩ - bác sĩ",
+    position: "giám đốc chuyên môn da liễu",
     note: "Nội dung 5"
   },
   {
-    name: "Bác sĩ 6",
-    image: `${doctor_kn_2_0_0_url}images/bacsi-small.webp`,
+    name: "Jolie huỳnh",
+    image: `${doctor_kn_2_0_0_url}images/bacsi-small5.webp`,
     picture: `${doctor_kn_2_0_0_url}images/bacsi.webp`,
-    level: "Thạc sĩ - Bác sĩ",
-    position: "Giám đốc chuyên môn Da liễu",
-    note: "Nội dung 6"
-  }
+    level: "thạc sĩ - bác sĩ",
+    position: "giám đốc chuyên môn da liễu",
+    note: "Nội dung 5"
+  },
   // Thêm các thông tin của bác sĩ khác vào đây
 ];
