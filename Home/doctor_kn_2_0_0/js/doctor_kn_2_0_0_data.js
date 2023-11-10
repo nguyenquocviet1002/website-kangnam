84ff3cbbf2d171e41e914948bd85dcd54de48ebc
nguyenquocviet1002 <vietnguyen10022001@gmail.com>
2023-11-10 16:41:48 +0700

    update

--- Home/doctor_kn_2_0_0/js/doctor_kn_2_0_0_data.js
+++ Home/doctor_kn_2_0_0/js/doctor_kn_2_0_0_data.js
@@ -2,50 +2,42 @@ const doctors = [
   {
     name: "richard huy",
     image: `${doctor_kn_2_0_0_url}images/bacsi-small1.webp`,
-    picture: `${doctor_kn_2_0_0_url}images/bacsi.webp`,
+    picture: `${doctor_kn_2_0_0_url}images/bacsi1.webp`,
     level: "tiến sĩ - Bác sĩ",
     position: "Giám đốc chuyên môn pttm kangnam",
-    note: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat ratione voluptatum ipsum assumenda, reiciendis molestias tenetur dolor voluptates sit optio ex repellendus inventore veritatis. Quos sunt maxime recusandae debitis incidunt?"
+    note: "- Tiến sĩ y học - Đại học Y Hà Nội. <br> - 20 năm kinh nghiệm PTTM. <br> - Thành viên Hội phẫu thuật Tạo hình Thẩm mỹ Quốc tế (ISAPS)."
   },
   {
     name: "kim jeong hwang",
     image: `${doctor_kn_2_0_0_url}images/bacsi-small2.webp`,
-    picture: `${doctor_kn_2_0_0_url}images/bacsi.webp`,
+    picture: `${doctor_kn_2_0_0_url}images/bacsi2.webp`,
     level: "giáo sư - Bác sĩ",
     position: "cố vấn chuyên môn hệ thống",
-    note: "Nội dung 2"
+    note: "- Giáo sư Bệnh viện Đại học quốc gia Seoul. <br> - Giáo sư Đại học Y Khoa Inje và bệnh viện Đa khoa Paik. <br> - Chứng chỉ Bác sĩ phẫu thuật thẩm mỹ & Da liễu được cấp bởi Bộ Y Tế Việt Nam."
   },
   {
     name: "henry nguyễn",
     image: `${doctor_kn_2_0_0_url}images/bacsi-small3.webp`,
-    picture: `${doctor_kn_2_0_0_url}images/bacsi.webp`,
+    picture: `${doctor_kn_2_0_0_url}images/bacsi3.webp`,
     level: "bác sĩ ck1",
     position: "phó khoa PTTM gương mặt ",
-    note: "Nội dung 3"
+    note: "- Phó khoa Phẫu thuật thẩm mỹ gương mặt BVTM Kangnam. <br> - Trên 12 năm kinh nghiệm."
   },
   {
     name: "thủy lê",
     image: `${doctor_kn_2_0_0_url}images/bacsi-small4.webp`,
-    picture: `${doctor_kn_2_0_0_url}images/bacsi.webp`,
+    picture: `${doctor_kn_2_0_0_url}images/bacsi4.webp`,
     level: "Bác sĩ",
     position: "trưởng khoa da liễu",
-    note: "Nội dung 4"
+    note: "- Bác sĩ CK1 đại học Y Hà Nội. <br> - Trưởng khoa da liễu BVTM Kangnam. <br> - Có 20 năm kinh nghiệm trong lĩnh vực điều trị da."
   },
   {
     name: "Jolie huỳnh",
     image: `${doctor_kn_2_0_0_url}images/bacsi-small5.webp`,
-    picture: `${doctor_kn_2_0_0_url}images/bacsi.webp`,
+    picture: `${doctor_kn_2_0_0_url}images/bacsi5.webp`,
     level: "thạc sĩ - bác sĩ",
     position: "giám đốc chuyên môn da liễu",
-    note: "Nội dung 5"
-  },
-  {
-    name: "Jolie huỳnh",
-    image: `${doctor_kn_2_0_0_url}images/bacsi-small5.webp`,
-    picture: `${doctor_kn_2_0_0_url}images/bacsi.webp`,
-    level: "thạc sĩ - bác sĩ",
-    position: "giám đốc chuyên môn da liễu",
-    note: "Nội dung 5"
+    note: "- Thạc sĩ - bác sĩ chuyên khoa da liễu. <br> - Tốt nghiệp trường Đại học Y dược HCM. <br> - Chuyên gia da liễu cao cấp 20 năm kinh nghiệm trong lĩnh vực điều trị da."
   },
   // Thêm các thông tin của bác sĩ khác vào đây
 ];
