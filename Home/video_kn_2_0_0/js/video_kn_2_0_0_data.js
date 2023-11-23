800ee3d2a7432d230e4d88189e428cb37ab0845b
huudinh <dinhndvn@yahoo.com>
2023-11-23 10:08:55 +0700

    update Homepage

--- Home/video_kn_2_0_0/js/video_kn_2_0_0_data.js
+++ Home/video_kn_2_0_0/js/video_kn_2_0_0_data.js
@@ -2,24 +2,24 @@ const video_kn_2_0_0_data = [
     {
         category: 'Show Hành trình lột xác',
         description: 'Hành trình lột xác - show thẩm mỹ "đắt giá" được tổ chức bởi Hệ thống Thẩm mỹ Kangnam. Chương trình mang đậm tính nhân văn sâu sắc, giúp thí sinh “lột xác” với diện mạo mới, đón nhận nhiều cơ hội mới trong cuộc sống.',
-        thumb: `${video_kn_2_0_0_url}images/thumb-video.webp`,
+        thumb: `${video_kn_2_0_0_url}images/thumb-video.webp?v=1`,
         item: [
             {
                 season: 'Mùa 1',
                 episode: 'Tập 1',
-                thumb: `${video_kn_2_0_0_url}images/thumb.webp?ver=1`,
+                thumb: `${video_kn_2_0_0_url}images/thumb.webp?v=1`,
                 link: '/',
             },
             {
                 season: 'Mùa 1',
                 episode: 'Tập 2',
-                thumb: `${video_kn_2_0_0_url}images/thumb.webp?ver=1`,
+                thumb: `${video_kn_2_0_0_url}images/thumb.webp?v=1`,
                 link: '/',
             },
             {
                 season: 'Mùa 2',
                 episode: 'Tập 2',
-                thumb: `${video_kn_2_0_0_url}images/thumb.webp?ver=1`,
+                thumb: `${video_kn_2_0_0_url}images/thumb.webp?v=1`,
                 link: '/',
             },
         ]
@@ -27,24 +27,24 @@ const video_kn_2_0_0_data = [
     {
         category: 'Show Beauty Up',
         description: 'Show Beauty Up - show thẩm mỹ "đắt giá" được tổ chức bởi Hệ thống Thẩm mỹ Kangnam. Chương trình mang đậm tính nhân văn sâu sắc, giúp thí sinh “lột xác” với diện mạo mới, đón nhận nhiều cơ hội mới trong cuộc sống.',
-        thumb: `${video_kn_2_0_0_url}images/thumb-video.webp`,
+        thumb: `${video_kn_2_0_0_url}images/thumb-video.webp?v=1`,
         item: [
             {
                 season: 'Mùa 1',
                 episode: 'Tập 1',
-                thumb: `${video_kn_2_0_0_url}images/thumb.webp?ver=1`,
+                thumb: `${video_kn_2_0_0_url}images/thumb.webp?v=1`,
                 link: '/',
             },
             {
                 season: 'Mùa 1',
                 episode: 'Tập 2',
-                thumb: `${video_kn_2_0_0_url}images/thumb.webp?ver=1`,
+                thumb: `${video_kn_2_0_0_url}images/thumb.webp?v=1`,
                 link: '/',
             },
             {
                 season: 'Mùa 2',
                 episode: 'Tập 1',
-                thumb: `${video_kn_2_0_0_url}images/thumb.webp?ver=1`,
+                thumb: `${video_kn_2_0_0_url}images/thumb.webp?v=1`,
                 link: '/',
             },
         ]
@@ -52,30 +52,30 @@ const video_kn_2_0_0_data = [
     {
         category: 'Giải cứu mũi hỏng',
         description: 'Giải cứu mũi hỏng - show thẩm mỹ "đắt giá" được tổ chức bởi Hệ thống Thẩm mỹ Kangnam. Chương trình mang đậm tính nhân văn sâu sắc, giúp thí sinh “lột xác” với diện mạo mới, đón nhận nhiều cơ hội mới trong cuộc sống.',
-        thumb: `${video_kn_2_0_0_url}images/thumb-video.webp`,
+        thumb: `${video_kn_2_0_0_url}images/thumb-video.webp?v=1`,
         item: [
             {
                 season: 'Mùa 2',
                 episode: 'Tập 1',
-                thumb: `${video_kn_2_0_0_url}images/thumb.webp?ver=1`,
+                thumb: `${video_kn_2_0_0_url}images/thumb.webp?v=1`,
                 link: '/',
             },
             {
                 season: 'Mùa 3',
                 episode: 'Tập 2',
-                thumb: `${video_kn_2_0_0_url}images/thumb.webp?ver=1`,
+                thumb: `${video_kn_2_0_0_url}images/thumb.webp?v=1`,
                 link: '/',
             },
             {
                 season: 'Mùa 2',
                 episode: 'Tập 2',
-                thumb: `${video_kn_2_0_0_url}images/thumb.webp?ver=1`,
+                thumb: `${video_kn_2_0_0_url}images/thumb.webp?v=1`,
                 link: '/',
             },
             {
                 season: 'Mùa 2',
                 episode: 'Tập 3',
-                thumb: `${video_kn_2_0_0_url}images/thumb.webp?ver=1`,
+                thumb: `${video_kn_2_0_0_url}images/thumb.webp?v=1`,
                 link: '/',
             },
         ]
