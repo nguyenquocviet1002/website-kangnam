253feaf738e6d9ea314ee1b29b7378b76ddcc419
nguyenquocviet1002 <vietnguyen10022001@gmail.com>
2023-12-27 16:08:00 +0700

    update

--- Home/slider_kn_2_0_0/js/slider_kn_2_0_0_data.js
+++ Home/slider_kn_2_0_0/js/slider_kn_2_0_0_data.js
@@ -1,5 +1,10 @@
 const dataSlide1 = [
     {
+        pc: `https://benhvienthammykangnam.vn/wp-content/uploads/2023/12/slide-2712.jpg`,
+        mb: `https://benhvienthammykangnam.vn/wp-content/uploads/2023/12/slide-2712-m.jpg`,
+        link: 'https://benhvienthammykangnam.vn/kien-thuc-lam-dep/uu-dai/',
+    },
+    {
         pc: `${slider_kn_2_0_0_url}images/slide1.webp?ver=3`,
         mb: `${slider_kn_2_0_0_url}images/slide1-mb.webp?ver=3`,
         link: '#',
@@ -8,10 +13,5 @@ const dataSlide1 = [
         pc: `${slider_kn_2_0_0_url}images/slide2.webp?ver=3`,
         mb: `${slider_kn_2_0_0_url}images/slide2-mb.webp?ver=3`,
         link: 'https://benhvienthammykangnam.vn/tin-tuc-thuong-hieu/kangnam-13-nam-tan-tam-nang-tam-nhan-sac-viet/',
-    },
-    {
-        pc: `${slider_kn_2_0_0_url}images/slide3.webp?ver=3`,
-        mb: `${slider_kn_2_0_0_url}images/slide3-mb.webp?ver=3`,
-        link: 'https://benhvienthammykangnam.com.vn/uu-dai/',
     },
 ];
