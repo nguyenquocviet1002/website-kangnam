d8b4b756b0af97153c7368c606d12a29c690cccb
quangphe <minhquang1356@gmail.com>
2024-05-03 17:10:33 +0700

    update gioi thieu

---
+++ Pages/gioi-thieu/js/gioi-thieu.js
@@ -0,0 +1,196 @@
+const dataMedia = [
+    {
+        title: '5 ca phẫu thuật khó tại bệnh viện thẩm mỹ Kangnam',
+        brand: 'vnexpress.net',
+        logo: 'media/images/logo-vnexpress.svg',
+        post: [
+            {
+                title: '5 ca phẫu thuật khó tại bệnh viện thẩm mỹ Kangnam',
+                content: `
+                <p>Nhiều bệnh nhân có gương mặt biến dạng, phải trải qua nhiều ca phẫu thuật phức tạp như "cô gái mặt lõm" Thu Hường hay Diệu Thúy với khối u lớn trên mặt.</p>
+                <p>Bệnh viện Thẩm mỹ Kangnam đã thực hiện những ca phẫu thuật phức tạp, đòi hỏi kết hợp nhiều kỹ thuật. Dưới đây là 5 ca thẩm mỹ khó được thực hiện thành công.</p>
+                <h2>Ca giải cứu mặt lõm cho Ninh Thị Thu Hường</h2>
+                <p>Thu Hường, "cô gái mặt lõm" đến từ Ninh Bình đã trải qua hành trình lột xác và biến đổi sau phẫu thuật tại <a href="">bệnh viện Thẩm mỹ Kangnam</a>. Với mặt lõm do di chứng cắt khối u khi còn nhỏ, sau chụp chiếu, xét nghiệm, các bác sĩ xác định tình trạng biến dạng của Hường đặc biệt phức tạp, không chỉ nằm ở da mà còn biến dạng xương, phần mô cơ và toàn bộ phần mô mềm ở góc hàm và cổ.</p>
+                <p><img src="https://i1-suckhoe.vnecdn.net/2024/04/26/1000012665-8898-1714104303.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=UJs10S6Zq64frSkSk5YJ6w" alt="" /></p>
+                `,
+                date: '26/4/2024',
+                path: 'https://vnexpress.net/5-ca-phau-thuat-kho-tai-benh-vien-tham-my-kangnam-4738789.html'
+            }
+        ]
+        
+    },
+    {
+        title: 'Khát khao được ‘lấp đầy’ của cô gái mặt lõm',
+        brand: 'tuoitre.vn',
+        logo: 'media/images/logo-tuoi-tre.svg',
+        post: [
+            {
+                title: 'Khát khao được ‘lấp đầy’ của cô gái mặt lõm',
+                content: `
+                <h2>Gần 20 năm sống với ngoại hình ‘khác thường’, chưa ngày nào Thu Hường thôi khát khao được ‘lấp đầy’ để có được gương mặt ưa nhìn hơn.</h2>
+                <p>Thu Hường (22 tuổi) sinh ra và lớn lên trong một gia đình thuần nông ở Ninh Bình với bố mẹ và em trai. Vốn sinh ra có hình hài bình thường, xinh xắn những tưởng cuộc sống của cô gái cứ thế êm đềm trôi qua nhưng 3 tuổi gia đình phát hiện Hường có một khối u nhỏ bằng đầu ngón tay ở dái tai.</p>
+                <p>Khăn gói lên Hà Nội thăm khám, gia đình gần như sụp đổ khi bác sĩ kết luận Hường bị u xương hàm ác tính. Do Hường còn quá bé và nỗi sợ ‘bị K lại đụng chạm dao kéo sẽ chết’ nên bố mẹ quyết định cho Hường về quê.</p>
+                <p><img src="https://i1-suckhoe.vnecdn.net/2024/04/26/1000012665-8898-1714104303.jpg?w=680&h=0&q=100&dpr=1&fit=crop&s=UJs10S6Zq64frSkSk5YJ6w" alt="" /></p>
+                `,
+                date: '09/04/2024',
+                path: 'https://tuoitre.vn/khat-khao-duoc-lap-day-cua-co-gai-mat-lom-2024040818411557.htm'
+            }
+        ]
+        
+    },
+    {
+        title: 'Kangnam hiện thực hóa ước mơ lan tỏa vẻ đẹp tới mọi miền Việt Nam',
+        brand: 'soha.vn',
+        logo: 'media/images/logo-soha.svg',
+        post: [
+            {
+                title: 'Kangnam hiện thực hóa ước mơ lan tỏa vẻ đẹp tới mọi miền Việt Nam',
+                content: `
+                <h2>Thực hiện lan tỏa vẻ đẹp tới mọi miền, Kangnam mở rộng hệ thống viện thẩm mỹ chuẩn Hàn trải dài trên cả nước và tài trợ thẩm mỹ miễn phí 10 tỷ đồng cho các hoạt động thẩm mỹ thiện nguyện tại các tỉnh.</h2>
+                <p><img src="https://sohanews.sohacdn.com/thumb_w/660/pr/2023/photo1694159731244-1694159731350451468984-63829786174129.png" alt="" /></p>
+                <p>Hành trình lột xác - chương trình thẩm mỹ "đắt giá"</p>
+                <p>Hành trình lột xác là cái tên quen thuộc của các tín đồ làm đẹp. Không chỉ là chương trình thực tế đầu tiên về phẫu thuật thẩm mỹ tại Việt Nam, Hành trình lột xác còn chiếm trọn spotlight kể từ khi ra mắt, tạo được tiếng vang lớn trong công chúng, đưa thẩm mỹ làm đẹp đến gần hơn với mọi người. "Lột xác" không còn dừng lại ở tên chương trình mà đã trở thành cụm từ "quốc dân" để miêu tả sự thay đổi ngoạn mục.</p>
+                `,
+                date: '08/09/2023',
+                path: 'https://soha.vn/kangnam-hien-thuc-hoa-uoc-mo-lan-toa-ve-dep-toi-moi-mien-viet-nam-20230908160934059.htm'
+            }
+        ]
+        
+    },
+    {
+        title: 'Trải qua 4 cuộc đại phẫu, cô gái mặt lõm lột xác xinh như công chúa',
+        brand: 'kenh14.vn',
+        logo: 'media/images/logo-kenh14.svg',
+        post: [
+            {
+                title: 'Trải qua 4 cuộc đại phẫu, cô gái mặt lõm lột xác xinh như công chúa',
+                content: `
+                <h2>Do di chứng của u xương hàm, Ninh Thị Thu Hường (22 tuổi) phải sống với gương mặt khuyết lõm và biệt danh "cô gái mặt lõm" gần 20 năm. Trải qua 4 cuộc đại phẫu, Hường đã lột xác ngoạn mục, "hóa thân" thành cô công chúa xinh đẹp.</h2>
+                <p><img src="https://channel.mediacdn.vn/thumb_w/640/428462621602512896/2024/4/8/photo-1-1712561307303182344545.jpg" alt="" /></p>
+                <p>3 tuổi, Hường mọc cục u nhỏ và được bác sĩ chẩn đoán là u ác tính. Cứ thế đến khi lên 6, mặt Hường ngày một biến dạng. Bố mẹ quyết định phẫu thuật cắt bỏ khối u cho cô nhưng di chứng để lại là gương mặt ngày một lõm sâu. Mặt biến dạng khiến cô gái nhỏ luôn tự ti về ngoại hình của mình.</p>
+                <p>22 năm sống cùng gương mặt khuyết thiếu, cuộc sống tưởng mãi là một bức tranh đen tối thì bước ngoặt lớn đến với Thu Hường. Cô may mắn trở thành thí sinh giành được tấm vé vàng từ chương trình Hành trình Lột xác do Bệnh viện thẩm mỹ Kangnam tổ chức. Cô quyết tâm nắm bắt cơ hội thay đổi cuộc đời của mình.</p>
+                `,
+                date: '09/04/2024',
+                path: 'https://kenh14.vn/trai-qua-4-cuoc-dai-phau-co-gai-mat-lom-lot-xac-xinh-nhu-cong-chua-20240408215707254.chn'
+            }
+        ]
+        
+    },
+    {
+        title: 'Đan Trường xuất hiện với giao diện bắt mắt tại lễ khai trương Kangnam Buôn Ma Thuột',
+        brand: 'afamily.vn',
+        logo: 'media/images/logo-afamily.svg',
+        post: [
+            {
+                title: 'Đan Trường xuất hiện với giao diện bắt mắt tại lễ khai trương Kangnam Buôn Ma Thuột',
+                content: `
+                <h2>Sáng ngày 1/10, ca sĩ Đan Trường, Trung Quang và vũ đoàn Lido đã có buổi trình diễn sôi động tại sự kiện khai trương viện thẩm mỹ Kangnam Buôn Ma Thuột khiến hàng ngàn người dân Xứ Buôn Mê vô cùng thích thú.</h2>
+                <p><img src="https://afamilycdn.com/150157425591193600/2023/10/3/photo-1-1696306203508649090018-1696313135947-16963131369841902261235.png" alt="" /></p>
+                <p>Không chỉ "gây thương nhớ" cho các tin đồ làm đẹp tại sự kiện khai trương Kangnam Nghệ An, giọng ca Kiếp Ve Sầu tiếp tục khiến người dân Buôn Ma Thuột "thổn thức" với giọng ca ngọt ngào say đắm.</p>
+                <p>Sau nhiều ngày chờ đợi, Kangnam Buôn Ma Thuột chính thức khai trương. Sự kiện thu hút đông đảo người dân Buôn Mê Thuột tham dự. Họ mong chờ cơ hội được gặp thần tượng trong "gang tấc", được thăm khám miễn phí bởi các chuyên gia thẩm mỹ hàng đầu và nhận những ưu đãi hấp dẫn từ Kangnam.</p>
+                `,
+                date: '03/10/2023',
+                path: 'https://afamily.vn/dan-truong-xuat-hien-voi-giao-dien-bat-mat-tai-le-khai-truong-kangnam-buon-ma-thuot-20231003130454628.chn'
+            }
+        ]
+        
+    },
+    {
+        title: 'Kangnam đồng loạt khai trương hệ thống, tài trợ thẩm mỹ miễn phí 20 tỷ đồng',
+        brand: '24h.com.vn',
+        logo: 'media/images/logo-24h.svg',
+        post: [
+            {
+                title: 'Kangnam đồng loạt khai trương hệ thống, tài trợ thẩm mỹ miễn phí 20 tỷ đồng',
+                content: `
+                <h2>Nằm trong chương trình khai trương chuỗi viện thẩm mỹ chuẩn Hàn trên toàn quốc, Kangnam tặng 30 suất PTTM miễn phí cùng nhiều phần quà hấp dẫn với tổng giá trị lên đến 20 tỷ đồng. </h2>
+                <p>Xu hướng thẩm mỹ Hàn Quốc được xem là chuẩn mực làm đẹp tại Việt Nam và tính đến nay chưa hề có dấu hiệu hạ nhiệt. Thấu hiểu nhu cầu làm đẹp của chị em và nắm bắt xu thế làm đẹp đang được ưa chuộng trên thế giới, Kangnam phát triển hệ thống viện thẩm mỹ chuẩn Hàn với 3 bệnh viện và 10 viện thẩm mỹ tại 11 tỉnh thành trải dài cả 3 miền Bắc - Trung - Nam.</p>
+                <p>“Cơn mưa quà tặng” hấp dẫn</p>
+                <p><img src="https://icdn.24h.com.vn/upload/3-2023/images/2023-09-20/picture-7-1695182455-792-width1200height628.jpg" alt="" /></p>
+                `,
+                date: '20/09/2023',
+                path: 'https://www.24h.com.vn/bi-quyet-lam-dep/kangnam-dong-loat-khai-truong-he-thong-tai-tro-tham-my-mien-phi-20-ty-dong-c673a1503092.html'
+            }
+        ]
+        
+    },
+    {
+        title: '“Ông hoàng nhạc sến” Ngọc Sơn làm xao xuyến khán giả Hải Phòng',
+        brand: 'eva.vn',
+        logo: 'media/images/logo-eva.svg',
+        post: [
+            {
+                title: '“Ông hoàng nhạc sến” Ngọc Sơn làm xao xuyến khán giả Hải Phòng',
+                content: `
+                <h2>Ngày 8/10, giọng ca “Tình cha” góp mặt trong lễ khai trương Viện thẩm mỹ Kangnam Hải Phòng và hát tặng khán giả nhiều bài hát trữ tình ngọt ngào, làm lay động hàng ngàn người dân đất cảng. </h2>
+                <p>Dù đã U60 nhưng danh ca Ngọc Sơn luôn là một trong những cái tên hot, thu hút khán giả ở mọi độ tuổi. Từ sáng sớm, rất nhiều người dân Hải Phòng đã có mặt tại khách sạn Sheraton để chờ đợi sự xuất hiện của “ông hoàng nhạc sến”.</p>
+                <p><img src="https://cdn.eva.vn/upload/4-2023/images/2023-10-09/picture-1-1696824021-799-width1128height750.jpg" alt="" /></p>
+                <p>Đáp lại sự mong đợi của khán giả Hải Phòng, danh ca Ngọc Sơn hát tặng một loạt các bản nhạc trữ tình ngọt ngào, sâu lắng. Với giọng ca truyền cảm, nam danh ca làm xao xuyến biết bao trái tim người hâm mộ. Từng giai điệu quen thuộc của ca khúc: Lời tỏ tình dễ thương, Đồ Sơn... Khán giả cùng hòa chung tiếng hát, khiến không khí trở nên ấm cúng. </p>
+                `,
+                date: '09/10/2023',
+                path: 'https://eva.vn/lam-dep/ong-hoang-nhac-sen-ngoc-son-lam-xao-xuyen-khan-gia-hai-phong-c58a573336.html'
+            }
+        ]
+        
+    },
+    {
+        title: 'Hai ‘ông lớn’ ngành thẩm mỹ bắt tay ra mắt chuỗi chi nhánh tại hàng loạt tỉnh thành',
+        brand: 'tiin.vn',
+        logo: 'media/images/logo-tiin.png',
+        post: [
+            {
+                title: 'Hai ‘ông lớn’ ngành thẩm mỹ bắt tay ra mắt chuỗi chi nhánh tại hàng loạt tỉnh thành',
+                content: `
+                <h2>Mới đây, bệnh viện thẩm mỹ Kangnam cùng hệ thống thẩm mỹ viện Đông Á đã ký kết hợp tác toàn diện, phát triển 13 chi nhánh (3 bệnh viện thẩm mỹ và 10 viện thẩm mỹ) ở 11 tỉnh thành.</h2>
+                <p><img src="https://newsmd2fr.keeng.vn/tiin/archive/imageslead/2023/09/07/90_4090168a23d75f1c85ad69bbc002cb47.png" alt="" /></p>
+                <p>Lễ ký kết hợp tác đánh dấu bước phát triển mới của hai thương hiệu thẩm mỹ. Theo đó, lộ trình đến hết tháng 11, hệ thống Kangnam sẽ đồng loạt khai trương thêm 1 Bệnh viện thẩm mỹ tại TP.HCM và 10 Viện thẩm mỹ tại các thành phố Hà Nội, Bình Dương, Cần Thơ, Buôn Ma Thuột, Đà Nẵng, Nghệ An, Thanh Hóa, Hải Phòng, Bắc Ninh, Thái Nguyên.</p>
+                <p>Như vậy, thương hiệu này sẽ có mặt tại hầu hết các thành phố lớn ở cả ba miền Bắc - Trung – Nam.</p>
+                `,
+                date: '07/09/2023',
+                path: 'https://tiin.vn/chuyen-muc/sanh/hai-ong-lon-nganh-tham-my-bat-tay-ra-mat-chuoi-chi-nhanh-tai-hang-loat-tinh-thanh.html'
+            }
+        ]
+        
+    },
+]
+
+document.addEventListener('DOMContentLoaded', function() {
+    const mediaLinks = document.querySelectorAll('.introduce_kn_2_0_0__logoMedia');
+    const mediaLeftBox = document.querySelector('.introduce_kn_2_0_0__boxMediaLeft');
+
+    // Hàm hiển thị dữ liệu tương ứng khi click vào logo
+    function displayMediaData(index) {
+        const media = dataMedia[index];
+        let htmlContent = '';
+
+        media.post.forEach(post => {
+            htmlContent += `
+                <div class="introduce_kn_2_0_0__postBox">
+                    <h1 class="introduce_kn_2_0_0__postTitle">${post.title}</h1>
+                    <p class="introduce_kn_2_0_0__postDate">${post.date} - Nguồn: <span> ${media.brand} <span> </p>
+                    <div class="introduce_kn_2_0_0__postContent">${post.content}</div>
+                    <a href="${post.path}" target="_blank" class="introduce_kn_2_0_0__postPath">Xem thêm</a>
+                </div>
+            `;
+        });
+
+        mediaLeftBox.innerHTML = htmlContent;
+    }
+
+    // Thêm sự kiện click cho mỗi logo
+    mediaLinks.forEach((link, index) => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Ngăn chặn hành vi mặc định của thẻ a
+            displayMediaData(index);
+        });
+    });
+
+    // Hiển thị bài viết đầu tiên khi trang được tải
+    displayMediaData(0);
+});
+
+
+
+
+
+
