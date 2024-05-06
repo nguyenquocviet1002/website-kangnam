f6d6c64eb38cb67e85bde0597a0fa74ebe6c85e2
nguyenquocviet1002 <vietnguyen10022001@gmail.com>
2024-05-06 14:45:32 +0700

    update

--- Home/video_kn_2_0_0/js/video_kn_2_0_0_data.js
+++ Home/video_kn_2_0_0/js/video_kn_2_0_0_data.js
@@ -8,28 +8,33 @@ const video_kn_2_0_0_data = [
                 season: 'Mùa 6',
                 episode: 'Tập 2',
                 link: 'https://benhvienthammykangnam.vn/hanh-trinh-lot-xac-kangnam/hanh-trinh-lot-xac-mua-6-tap-2-tran-thi-xuan-mai/',
-                thumb: 'https://benhvienthammykangnam.vn/wp-content/uploads/2024/04/thumb-vid-xuan-mai.jpg'
+                thumb: 'https://benhvienthammykangnam.vn/wp-content/uploads/2024/04/thumb-vid-xuan-mai.jpg',
+                alt: 'Hành Trình Lột Xác mùa 6 | Tập 2: Trần Thị Xuân Mai – Cô bé vương miện sắt',
             },
             {
                 season: 'Mùa 6',
                 episode: 'Tập 1',
                 link: 'https://benhvienthammykangnam.vn/hanh-trinh-lot-xac-kangnam/hanh-trinh-lot-xac-mua-6-tap-1-ninh-thi-thu-huong/',
-                thumb: 'https://benhvienthammykangnam.vn/wp-content/uploads/2024/04/thumb-th.jpg'
+                thumb: 'https://benhvienthammykangnam.vn/wp-content/uploads/2024/04/thumb-th.jpg',
+                alt: 'Hành Trình Lột Xác mùa 6 | Tập 1 Ninh Thị Thu Hường – Cô gái mặt lõm',
             },
             {
                 season: 'Mùa 5',
                 episode: 'Tập 1',
                 link: 'https://www.youtube.com/watch?v=LlNsk4aCDgU&list=PLROTewhS4-MbT2mRXeL80UBeJlAQ_Fp9s&index=3',
+                alt: '',
             },
             {
                 season: 'Mùa 5',
                 episode: 'Tập 2',
                 link: 'https://www.youtube.com/watch?v=YREI2_aGd7I&list=PLROTewhS4-MbT2mRXeL80UBeJlAQ_Fp9s&index=4',
+                alt: '',
             },
             {
                 season: 'Mùa 5',
                 episode: 'Tập 3',
                 link: 'https://www.youtube.com/watch?v=wJrM3rEx1pY&list=PLROTewhS4-MbT2mRXeL80UBeJlAQ_Fp9s&index=5',
+                alt: '',
             },
         ]
     },
@@ -43,16 +48,19 @@ const video_kn_2_0_0_data = [
                 episode: 'Tập 1',
                 thumb: `${video_kn_2_0_0_url}images/thumb.webp?v=1`,
                 link: 'https://www.youtube.com/watch?v=eAIudrFNEKg&list=PLROTewhS4-MYcFAX_cJVY9GWnrqHR5M8u',
+                alt: 'Beauty Up | Tập 1: Ngọc Huyền - Vẫn là sự thật nhưng khuếch đại lên',
             },
             {
                 season: 'Mùa 1',
                 episode: 'Tập 2',
                 link: 'https://www.youtube.com/watch?v=7syiXqDLfI8&list=PLROTewhS4-MYcFAX_cJVY9GWnrqHR5M8u&index=2',
+                alt: 'Beauty Up | Tập 2: Phương Anh - Những nốt bổng của nhan sắc',
             },
             {
                 season: 'Mùa 1',
                 episode: 'Tập 3',
                 link: 'https://www.youtube.com/watch?v=iNUiO3V3Oz0&list=PLROTewhS4-MYcFAX_cJVY9GWnrqHR5M8u&index=3',
+                alt: 'Beauty Up | Tập 3: Huy Ngọc - 40 tuổi trốn chồng đi thẩm mỹ và cái kết',
             },
         ]
     },
@@ -65,16 +73,19 @@ const video_kn_2_0_0_data = [
                 season: 'Mùa 1',
                 episode: 'Tập 1',
                 link: 'https://www.youtube.com/watch?v=2Gvrri4VZOw&list=PLROTewhS4-MaQ8Fb6pseOa2Py_nNDQ1-R&index=1',
+                alt: 'Đinh Nho Liêm và câu chuyện cảm động về việc giải cứu mũi cong vẹo',
             },
             {
                 season: 'Mùa 1',
                 episode: 'Tập 2',
                 link: 'https://www.youtube.com/watch?v=fCLVfMpUeR0&list=PLROTewhS4-MaQ8Fb6pseOa2Py_nNDQ1-R&index=2',
+                alt: 'Tư vấn sửa mũi hỏng, gãy xương chính mũi lâu năm | Giải cứu mũi hỏng',
             },
             {
                 season: 'Mùa 1',
                 episode: 'Tập 3',
                 link: 'https://www.youtube.com/watch?v=iCdaVXNX_ig&list=PLROTewhS4-MaQ8Fb6pseOa2Py_nNDQ1-R&index=3',
+                alt: 'Sửa mũi hỏng, tụt sụn do tai nạn | Giải cứu mũi hỏng',
             },
         ]
     },
